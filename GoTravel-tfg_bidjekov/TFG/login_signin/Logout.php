a69af1ffeb39807fd1e8dac25938ba69dd0c75cb
Ricardo <133355632+Ricardotb2000@users.noreply.github.com>
2024-11-25 12:58:14 +0100

    Delete GoTravel-tfg_bidjekov/TFG/login_signin/Logout.php

--- GoTravel-tfg_bidjekov/TFG/login_signin/Logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // Inicia la sesión
-session_unset(); // Libera todas las variables de sesión
-session_destroy(); // Destruye la sesión
-header('Location: ../Index.php'); // Redirige a la página principal
-exit;
-?>
